688bdc4fb2f9d7e879aa90ce0604c512f92f969f
BUTCHERS228 <snowstormz@yandex.ru>
2021-12-29 00:23:19 +0400

    Показ выдвигаемых только для тех, у кого есть права

--- resources/views/livewire/admin/conv/tableconv.blade.php
+++ resources/views/livewire/admin/conv/tableconv.blade.php
@@ -6,7 +6,8 @@
             </div>
             <div class="card-content">
                 <div class="card-body">
-                    <p class="card-text">бла бла бла <code> и еще бла бла</code>
+                    <p class="card-text">Голосовать обязаны все без исключения. <code>Инициатора голосования видны
+                            6+</code>
                     </p>
                     <!-- Table with outer spacing -->
                     <div class="table-responsive">
@@ -16,7 +17,9 @@
                                 <th>Ник</th>
                                 <th>Тип</th>
                                 <th>Дата начала</th>
+                                @if(Auth::user()->hasAccess("watch_convers"))
                                 <th>Кто начал</th>
+                                @endif
                                 <th>Статистика</th>
                                 <th>Действие</th>
                             </tr>
@@ -27,12 +30,14 @@
                                     <td class="text-bold-500">{{ $key->nickname }}</td>
                                     <td>{{ $key->type}}</td>
                                     <td class="text-bold-500">{{ $key->created_at->diffForHumans() }}</td>
+                                    @if(Auth::user()->hasAccess("watch_convers"))
                                     <td class="text-bold-500">
                                         <div class="avatar avatar-lg me-3">
                                             <img src="{{ $key->profile->avatar }}" alt="" srcset="">
                                         </div>
                                         <a class="font-bold ms-3 mb-0">{{ $key->profile->name }}
                                             ({{ $key->profile->nickname }})</a></td>
+                                    @endif
                                     <td class="text-bold-500"><span style="color: #47a76a">{{ $key->convVote->sum('agree') }}</span> / <span style="color: #CB4154">{{ $key->convVote->sum('disagree') }}</span>
                                         / <span style="color: #B5B8B1;">{{ $key->convVote->sum('neutral') }}</span></td>
                                     <td><a href="{{ route('adminconv', ['id' => $key->id]) }}" class="btn btn-primary">Перейти</a></td>
