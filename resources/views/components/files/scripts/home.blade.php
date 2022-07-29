ec08a105b1b713de025ed7abd63ace5cc9fdf6d7
Aleksandr <snowstormz@yandex.ru>
2022-07-30 00:02:29 +0300

    Добавлена страница скриптов для пользователей

---
+++ resources/views/components/files/scripts/home.blade.php
@@ -0,0 +1,10 @@
+@extends('base')
+@section('header')
+    @include('components.homepage.header')
+@endsection
+@section('title')
+    <title>ZEROTWO | FILES</title>
+@endsection
+@section('content')
+    @include('components.files.scripts.add')
+@endsection
