c21932f18e2c4e5f5efc21c1f54993e20016a267
Aleksandr <snowstormz@yandex.ru>
2022-07-29 23:58:42 +0300

    Добавлен компонент livewire таблицы с данными игры за неделю

---
+++ resources/views/components/time/table.blade.php
@@ -0,0 +1,20 @@
+<div class="col-md-6 col-12">
+    <div class="card">
+        <div class="card-header">
+            <h4 class="card-title">Учет игрового времени</h4>
+        </div>
+        <div class="card-content">
+            <div class="card-body">
+                <form class="form form-horizontal">
+                    <div class="form-body">
+                        <div class="row">
+                            <div class="table-responsive">
+                                @livewire('time.table')
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
