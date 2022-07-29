3b869a2d423ea86c134b34411dd5701cb9e011d5
Aleksandr <snowstormz@yandex.ru>
2022-07-29 23:58:35 +0300

    Добавлен компонент livewire таблицы с данными игры за неделю

---
+++ resources/views/livewire/time/table.blade.php
@@ -0,0 +1,16 @@
+<table class="table table-lg">
+    <thead>
+    <tr>
+        <th>Игровой ник</th>
+        <th>Уровень</th>
+        <th>Время игры</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td class="text-bold-500">Michael Right</td>
+        <td>$15/hr</td>
+        <td class="text-bold-500">UI/UX</td>
+    </tr>
+    </tbody>
+</table>
