186a2dc85f426bbf661fb674c52dd152b5961f12
Aleksandr <snowstormz@yandex.ru>
2022-07-30 00:04:09 +0300

    Добавлен компонент livewire - поиск данных по времени недели

---
+++ resources/views/components/time/card.blade.php
@@ -0,0 +1,12 @@
+<div class="col-md-6 col-12">
+    <div class="card">
+        <div class="card-header">
+            <h4 class="card-title">Выбор недели</h4>
+        </div>
+        <div class="card-content">
+            <div class="card-body">
+                @livewire('time.card')
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/livewire/time/card.blade.php
@@ -0,0 +1,16 @@
+<form class="form form-horizontal">
+    <div class="form-body">
+        <div class="row">
+            <fieldset class="form-group">
+                <select class="form-select" id="basicSelect">
+                    <option>IT</option>
+                    <option>Blade Runner</option>
+                    <option>Thor Ragnarok</option>
+                </select>
+            </fieldset>
+            <div class="col-12 d-flex justify-content-end">
+                <button type="submit" class="btn btn-primary me-1 mb-1">Применить</button>
+            </div>
+        </div>
+    </div>
+</form>
