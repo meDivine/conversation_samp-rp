d562c4d9288a5a30f610cfb92ab17fda2864b0cd
Aleksandr <snowstormz@yandex.ru>
2022-07-30 12:11:02 +0300

    Добавлена таблица со слушателем

--- app/Http/Livewire/Time/Table.php
+++ app/Http/Livewire/Time/Table.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Livewire\Time;
 
+use App\Models\AdminTime;
+use App\Models\Week;
 use Livewire\Component;
 
 class Table extends Component
 {
+
+    public $table;
+
+    protected $listeners = ['week_get'];
+
+    public function week_get($week) {
+        $this->table = Week::query()->find($week);
+    }
+
     public function render()
     {
-        return view('livewire.time.table');
+        return view('livewire.time.table', ['unionTable' => $this->table]);
     }
 }
