35889e6b34f10ed0831b59e18106b115ecf6f651
Aleksandr <snowstormz@yandex.ru>
2022-07-30 12:10:42 +0300

    Добавлен слушатель селекта

--- app/Http/Livewire/Time/Card.php
+++ app/Http/Livewire/Time/Card.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Time;
 
+use App\Models\Week;
 use Livewire\Component;
 
 class Card extends Component
@@ -9,8 +10,15 @@ class Card extends Component
 
     public $date;
 
+    public function updated()
+    {
+        $this->emit('week_get', $this->date);
+    }
+
     public function render()
     {
-        return view('livewire.time.card');
+        return view('livewire.time.card', [
+            'weeks' => Week::query()->orderBy('week_start')->get()
+        ]);
     }
 }
