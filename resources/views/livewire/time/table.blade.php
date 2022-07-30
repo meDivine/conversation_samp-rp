2355ef8132ee2b8d8cf869f9763dc216a416bf9c
Aleksandr <snowstormz@yandex.ru>
2022-07-30 12:10:53 +0300

    Добавлена таблица с разграниченеием цветов онлайна

--- resources/views/livewire/time/table.blade.php
+++ resources/views/livewire/time/table.blade.php
@@ -3,14 +3,32 @@
     <tr>
         <th>Игровой ник</th>
         <th>Уровень</th>
-        <th>Время игры</th>
+        <th>Часы</th>
+        <th>Минуты</th>
+        <th>Секунды</th>
     </tr>
     </thead>
     <tbody>
-    <tr>
-        <td class="text-bold-500">Michael Right</td>
-        <td>$15/hr</td>
-        <td class="text-bold-500">UI/UX</td>
-    </tr>
+    @if (isset($unionTable) && !empty($unionTable->weekInfo))
+        @foreach($unionTable->weekInfo as $key)
+            @if($key->hours < $unionTable->normal_play)
+            <tr style="color: red">
+                <td class="text-bold-500">{{ $key->nickname }}</td>
+                <td>{{ $key->level }}</td>
+                <td class="text-bold-500">{{ $key->hours }}</td>
+                <td class="text-bold-500">{{ $key->minutes }}</td>
+                <td class="text-bold-500">{{ $key->seconds }}</td>
+            </tr>
+            @else
+                <tr style="color: green">
+                    <td class="text-bold-500">{{ $key->nickname }}</td>
+                    <td>{{ $key->level }}</td>
+                    <td class="text-bold-500">{{ $key->hours }}</td>
+                    <td class="text-bold-500">{{ $key->minutes }}</td>
+                    <td class="text-bold-500">{{ $key->seconds }}</td>
+                </tr>
+            @endif
+        @endforeach
+    @endif
     </tbody>
 </table>
