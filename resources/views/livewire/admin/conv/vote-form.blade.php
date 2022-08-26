c408ddde0b51a396ae3623305b737595b0ebcf9d
Aleksandr <snowstormz@yandex.ru>
2022-08-27 02:15:31 +0300

    Добавил кнопку закрытия голосования га с проверкой на права в форму

--- resources/views/livewire/admin/conv/vote-form.blade.php
+++ resources/views/livewire/admin/conv/vote-form.blade.php
@@ -3,44 +3,44 @@
         <label for="disabledInput">Голос</label>
         <p>
             <li class="d-inline-block me-2 mb-1">
-                <div class="form-check">
-                    <div class="custom-control custom-checkbox" wire:click="disagreeUpdate">
-                        <input type="checkbox"
-                               class="form-check-input form-check-danger"
-                               name="customCheck" id="customColorCheck4"
-                               wire:model="disagree" >
-                        <label class="form-check-label"
-                               for="customColorCheck4">Отрицательный</label>
-                    </div>
-                </div>
-            </li>
-        </p>
-        <p>
-            <li class="d-inline-block me-2 mb-1" >
-                <div class="form-check">
-                    <div class="custom-control custom-checkbox" wire:click="neutralUpdate">
-                        <input type="checkbox" class="form-check-input form-check-info"
-                               name="customCheck" id="customColorCheck5"
-                               wire:model="neutral">
-                        <label class="form-check-label"
-                               for="customColorCheck5" >Нейтральный</label>
-                    </div>
-                </div>
-            </li>
+        <div class="form-check">
+            <div class="custom-control custom-checkbox" wire:click="disagreeUpdate">
+                <input type="checkbox"
+                       class="form-check-input form-check-danger"
+                       name="customCheck" id="customColorCheck4"
+                       wire:model="disagree">
+                <label class="form-check-label"
+                       for="customColorCheck4">Отрицательный</label>
+            </div>
+        </div>
+        </li>
         </p>
         <p>
             <li class="d-inline-block me-2 mb-1">
-                <div class="form-check">
-                    <div class="custom-control custom-checkbox" wire:click="agreeUpdate">
-                        <input type="checkbox"
-                               class="form-check-input form-check-success"
-                               name="customCheck" id="customColorCheck3"
-                               wire:model="agree">
-                        <label class="form-check-label"
-                               for="customColorCheck3" >Положительный</label>
-                    </div>
-                </div>
-            </li>
+        <div class="form-check">
+            <div class="custom-control custom-checkbox" wire:click="neutralUpdate">
+                <input type="checkbox" class="form-check-input form-check-info"
+                       name="customCheck" id="customColorCheck5"
+                       wire:model="neutral">
+                <label class="form-check-label"
+                       for="customColorCheck5">Нейтральный</label>
+            </div>
+        </div>
+        </li>
+        </p>
+        <p>
+            <li class="d-inline-block me-2 mb-1">
+        <div class="form-check">
+            <div class="custom-control custom-checkbox" wire:click="agreeUpdate">
+                <input type="checkbox"
+                       class="form-check-input form-check-success"
+                       name="customCheck" id="customColorCheck3"
+                       wire:model="agree">
+                <label class="form-check-label"
+                       for="customColorCheck3">Положительный</label>
+            </div>
+        </div>
+        </li>
         </p>
     </div>
 
@@ -55,4 +55,7 @@
                name="fname" wire:model="comment" placeholder="Видно тебе и ГА">
     </div>
     <button type="submit" class="btn btn-primary me-1 mb-1">Отправить</button>
+    @hasAccess('make_perm')
+    <button wire:click="closeConv" class="btn btn-danger me-1 mb-1">Закрыть</button>
+    @endhasAccess
 </form>
