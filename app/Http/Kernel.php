1d0d0ba04696a0ab7badd146a1658c21424bcd9e
Aleksandr <snowstormz@yandex.ru>
2022-08-27 02:14:04 +0300

    Регистрация посредника на проверку включенного голосования

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\ConvAccess;
+use App\Http\Middleware\ConvStartToggle;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -66,5 +67,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'can_conv' => ConvAccess::class,
         'access' => \Orchid\Platform\Http\Middleware\Access::class,
+        'convToggle' => ConvStartToggle::class,
     ];
 }
