77ed60c7334bfaeea0e46af093405ee5ce004595
Aleksandr <snowstormz@yandex.ru>
2022-08-27 02:14:27 +0300

    Изменение статуса голосование вкл/выкл

--- app/Orchid/Screens/PlatformScreen.php
+++ app/Orchid/Screens/PlatformScreen.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App\Orchid\Screens;
 
+use App\Orchid\Layouts\Home\HomeLayout;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Request;
 use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Screen;
 use Orchid\Support\Facades\Layout;
+use Orchid\Support\Facades\Toast;
 
 class PlatformScreen extends Screen
 {
@@ -64,7 +68,24 @@ class PlatformScreen extends Screen
     public function layout(): array
     {
         return [
-            Layout::view('platform::partials.welcome'),
+            HomeLayout::class
         ];
+    }
+
+    /**
+     * Ничего лучше не придумал)
+     * @return void
+     */
+    public function setConvStatus() {
+        if(Redis::get('convStat') == 0) {
+            Redis::set('convStat', 1);
+            Toast::info('Статус голосования на "Разрешено"');
+        }
+        else
+        {
+            Redis::set('convStat', 0);
+            Toast::info('Статус голосования на "Запрещено"');
+        }
+
     }
 }
