f4229bc192b16420280ee80e1873508fba73283e
Aleksandr <snowstormz@yandex.ru>
2022-08-31 11:49:54 +0300

    ipapi reint.

--- app/Classes/ipApi.php
+++ app/Classes/ipApi.php
@@ -17,7 +17,7 @@ class ipApi
 
     public function getIpInfo(): Response
     {
-        return Http::get("https://ip-api.com/json/$this->ip");
+        return Http::get("http://ip-api.com/json/$this->ip");
        // return Http::get("https://api.ipgeolocation.io/ipgeo?apiKey=7744c1ec2234467bb84832f4097b5072&ip=$this->ip");
     }
 
