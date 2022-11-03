9b8b3aab47ee642da70f75d0daff3f33e9fbd441
alexa <snowstormz@yandex.ru>
2022-11-03 12:49:54 +0300

    исправил тупость

--- routes/platform.php
+++ routes/platform.php
@@ -122,7 +122,7 @@ Route::screen('conversations', General::class)
 Route::screen('conversation/{conv}', Edit::class)
     ->name('platform.systems.conversation.edit');
 
-Route::screen('conversation/{conv}', UserVerifiedScreen::class)
+Route::screen('user/verified', UserVerifiedScreen::class)
     ->name('platform.user.verified');
 
 Route::screen('time', \App\Orchid\Screens\Time\Add::class)
