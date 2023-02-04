467a2c755bf48bb0c8d114c334bf89ef228dd2c3
alexa <snowstormz@yandex.ru>
2023-02-04 23:41:09 +0300

    api getwarns

--- app/Classes/Api/Warns.php
+++ app/Classes/Api/Warns.php
@@ -20,7 +20,7 @@ class Warns
      */
     public function getWarnLogs(): \Illuminate\Http\JsonResponse
     {
-        $logs = new Logs("punishments_api_search", $this->nick, null, '01.01.2014', Carbon::now()->format('d.m.Y'));
+        $logs = new Logs("punishments_api_search", $this->nick, null, '01.01.2010', Carbon::now()->format('d.m.Y'));
         return response()->json($logs->getLogs());
     }
 }
